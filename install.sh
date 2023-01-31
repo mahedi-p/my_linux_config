04930c2af488f2269dc457a1f9e2c8c8d5f20747
mahedi-p <124052858+mahedi-p@users.noreply.github.com>
2023-01-31 07:23:12 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,73 @@
+#!/bin/bash
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+# Change Debian to SID Branch
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp sources.list /etc/apt/sources.list
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Wallpaper
+cp .Xresources /home/$username
+cp .Xnord /home/$username
+cp -R dotconfig/* /home/$username/.config/
+cp bg.jpg /home/$username/Pictures/
+
+# Installing Essential Programs 
+nala install openbox rofi polybar picom pcmanfm nitrogen lxpolkit x11-xserver-utils unzip yad wget pulseaudio pavucontrol -y
+# Installing Other less important Programs
+nala install neofetch lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji -y
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd $builddir 
+nala install fonts-font-awesome
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+# Install chromium-browser
+sudo nala install chromium -y
+
+# Enable graphical login and change target from CLI to GUI
+systemctl enable lightdm
+systemctl set-default graphical.target
+
+# Polybar configuration
+bash scripts/changeinterface
